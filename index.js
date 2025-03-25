0dbf2b8d6acdadef97eaee5b0f8e7ebdef8995c2
shahbozhoca <upwork02092005@gmail.com>
2025-03-25 06:47:32 +0500

    animations and seo

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+window.addEventListener("scroll", function () {
+    const nav = document.querySelector(".nav");
+    if (window.scrollY > 300) {
+        nav.classList.add("scrolled");
+    } else if (window.scrollY > 150) {
+        nav.classList.add("scrolling");
+    } else {
+        nav.classList.remove("scrolled");
+        nav.classList.remove("scrolling");
+    }
+});
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const elements = document.querySelectorAll(".materials .list div");
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry, index) => {
             if (entry.isIntersecting) {
-                entry.target.style.transitionDelay = `${index * 0.2}s`; // Har biri 0.2s farq bilan chiqadi
+                entry.target.style.transitionDelay = `${index * 0.2}s`;
                 entry.target.classList.add("active");
             } else {
-                entry.target.classList.remove("active"); // Tepaga qaytganda class o‘chadi
+                entry.target.classList.remove("active");
             }
         });
     });
